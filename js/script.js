f5d0b7a071417d23fa7491e09768cda60ecb311f
Laura Markus <41956880+lauramarkus@users.noreply.github.com>
2021-11-15 18:55:04 +0100

    Adds function to assign pot luck items

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full 
 const guestFull = document.querySelector(".alert");
+//assign button
+const assignButton = document.querySelector(".assign");
+//list with guest name and dish
+const assignedItems = document.querySelector(".assigned-items")
 
 addGuestButton.addEventListener("click", function(){
     const guest = guestInput.value;
@@ -37,13 +41,23 @@ const addToList = function (guest){
 const updateGuestCount = function (){
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText=guests.length;
-    if (guests.length === 8){
+    if (guests.length == 3){
         addGuestButton.classList.add("hide");
         guestInput.classList.add("hide");
         guestInputLabel.classList.add("hide");
         guestFull.classList.remove("hide");
     }
     updateGuestCount();
-
 };
 
+const assignItems = function(){
+    const potluckItems = ["cheese", "olives", "fresh tomatoes and basil", "mozzarella", "prosciutto", "melon", "bread", "hummus and veggies", "pasta", "pesto", "wine", "fruit", "chocolate cake"];
+    const allGuests = document.querySelectorAll(".guest-list");
+    for (let guest of allGuests){
+        const randomPotluckIndex = Math.floor(Math.random) * potluckItems.length;
+        const randomPotluckItem = potluckItems[randomPotluckIndex];
+        const listItem = document.createElement("li");
+        listItem.innerText=`${guest.innerText} is bringing ${randomPotluckItem}`;
+        assignedItems.append(listItem);
+    }
+};
